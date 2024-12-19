c72f19af60ffad6ece1bfedb915eda200ac52369
aswin236 <aswinmuthuadm@gmail.com>
2024-12-20 01:11:20 +0530

    Update ex1.java

--- ex1.java
+++ ex1.java
@@ -1,1 +1,17 @@
-
+import java.util.scanner;
+class Main{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter an integer:");
+    int no = scanner.nextInt();
+    scanner.nextLine();
+    System.out.println("Enter a string:");
+    String text = scanner.nextLine();
+    System.out.println("Enter a character:");
+    char character=scanner.nextLine().charAt(0);
+    System.out.println(no);
+    System.out.println(text);
+    System.out.println(character);
+  }
+}
+    
