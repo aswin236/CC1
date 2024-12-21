57c90dd5fedae2601beacf0c538dd7b7ce2552b8
aswin236 <aswinmuthuadm@gmail.com>
2024-12-21 20:30:11 +0530

    Create Set2a.java

---
+++ Set2a.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Set2a {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input:");
+        int x=scan.nextInt();
+        int y=scan.nextInt();
+        for (int i=0; i<y; i++){
+            System.out.println(x);
+        }
+    }
+}
